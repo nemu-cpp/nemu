a8c4d94d253667e2144297a7afbc90b66a752ef1
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-02-20 19:54:39 +0000

    Add documentation

--- Include/NemuFramework/Nemu/Applications.h
+++ Include/NemuFramework/Nemu/Applications.h
@@ -30,12 +30,33 @@
 namespace Nemu
 {
 
+/// The list of applications in the program.
+/**
+    This class behaves as a singleton that stores the list of Application instances in the program. The constructors
+    and destructor of the Application class automatically add and remove instances to this class.
+    
+    On Windows, it is used by the Ctrl-C handler to stop all applications when a Ctrl-C event is received. But it can
+    be used for other purposes if needed.
+*/
 class Applications
 {
 public:
+    /// Stops all applications in the list.
     static void StopAllApplications();
 
+    /// Adds an application to the list.
+    /**
+        This function is automatically called by the Application constructors so there should be no need to call it
+        directly.
+        @param application The application to add to the list.
+    */
     static void set(Application* application);
+    // Removes an application from the list.
+    /**
+        This function is automatically called by the Application destructor so there should be no need to call it
+        directly.
+        @param application The application to remove from the list.
+    */
     static void unset(Application* application);
 
 private:
