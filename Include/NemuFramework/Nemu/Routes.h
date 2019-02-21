17d9d18eea61f33a784fd1773b712ba9d1d4c98d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-02-21 10:21:38 +0000

    Add documentation

--- Include/NemuFramework/Nemu/Routes.h
+++ Include/NemuFramework/Nemu/Routes.h
@@ -30,13 +30,31 @@
 namespace Nemu
 {
 
+/// A sequence of routes.
+/**
+    In addition to the routes explicitly added to the sequence a default route is also stored. It is returned by the
+    Routes::match function if no match is found.
+
+    This class is used by the Application class to store the route specifications. There should usually be no need to
+    create an instance of this class yourself.
+*/
 class Routes
 {
 public:
+    /// Constructor.
     Routes();
 
+    /// Appends a route to the sequence.
+    /**
+        @param route The route to add.
+    */
     void append(const Route& route);
 
+    /// Finds the first route in the sequence that matches the path.
+    /**
+        If no match is found the default route is returned.
+        @param path The path to match.
+    */
     const Route& match(const std::string& path) const;
 
 private:
