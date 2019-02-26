563753a053a809ba07dcb4a679b4dc8d8b474579
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-02-26 17:47:38 +0000

    Update main.cpp

--- Tests/Core/Source/main.cpp
+++ Tests/Core/Source/main.cpp
@@ -24,10 +24,15 @@
 #include "RoutesTests.h"
 #include "BeastTests/BeastTestSequence.h"
 #include "Ishiko/TestFramework/TestFrameworkCore.h"
+#include <boost/filesystem/operations.hpp>
 
 int main(int argc, char* argv[])
 {
     Ishiko::TestFramework::TestHarness theTestHarness("NemuCore");
+
+    theTestHarness.environment().setTestOutputDirectory("../../TestOutput");
+    boost::filesystem::create_directories("../../TestOutput");
+    theTestHarness.environment().setReferenceDataDirectory("../../ReferenceData");
 
     ConfigurationTests::AddTests(theTestHarness);
     RoutesTests::AddTests(theTestHarness);
