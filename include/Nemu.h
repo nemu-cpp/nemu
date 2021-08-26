66b2280633ff053612309417d8ef720d741af65c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-08-26 20:56:08 +0100

    Maintenance (#24)

---
+++ include/Nemu.h
@@ -0,0 +1,12 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/nemu/blob/main/LICENSE.txt
+*/
+
+#ifndef _NEMU_CPP_H_
+#define _NEMU_CPP_H_
+
+#include <Nemu/Core.h>
+
+#endif
