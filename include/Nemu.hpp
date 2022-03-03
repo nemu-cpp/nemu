67ecc8b35e0d2cf6b5e76875ac3dab4b6c73a6e1
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-03 14:43:59 +0000

    Updates

--- include/Nemu.hpp
+++ include/Nemu.hpp
@@ -1,11 +1,11 @@
 /*
-    Copyright (c) 2021 Xavier Leclercq
+    Copyright (c) 2021-2022 Xavier Leclercq
     Released under the MIT License
     See https://github.com/nemu-cpp/nemu/blob/main/LICENSE.txt
 */
 
-#ifndef _NEMU_CPP_H_
-#define _NEMU_CPP_H_
+#ifndef _NEMU_CPP_NEMU_HPP_
+#define _NEMU_CPP_NEMU_HPP_
 
 #include <Nemu/Core.h>
 
