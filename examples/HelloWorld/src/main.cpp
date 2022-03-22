851b3d8e8b62b7ccd3d8c00dfa959931445ea6d5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-22 23:54:05 +0000

    Fixes

--- examples/HelloWorld/src/main.cpp
+++ examples/HelloWorld/src/main.cpp
@@ -4,20 +4,23 @@
     See https://github.com/nemu-cpp/nemu/blob/main/LICENSE.txt
 */
 
+#include <Ishiko/HTTP.hpp>
 #include <Nemu.hpp>
 #include <iostream>
+#include <memory>
 
 int main(int argc, char* argv[])
 {
     Ishiko::Error error;
 
-    Nemu::IshikoServer server(Ishiko::TCPServerSocket::AllInterfaces, Ishiko::Port::http, error);
+    std::shared_ptr<Nemu::IshikoServer> server =
+        std::make_shared<Nemu::IshikoServer>(Ishiko::TCPServerSocket::AllInterfaces, Ishiko::Port::http, error);
 
     // Create a log that sends its output to the console.
     Ishiko::StreamLoggingSink sink(std::cout);
     Nemu::Logger log(sink);
 
-    Nemu::WebApplication app(log, error);
+    Nemu::WebApplication app(server, log, error);
     if (error)
     {
         std::cout << "Error: " << error << std::endl;
@@ -27,6 +30,8 @@ int main(int argc, char* argv[])
         [](const Nemu::WebRequest& request, Nemu::WebResponseBuilder& response, void* handlerData,
             Nemu::Logger& logger)
         {
+            // TODO: this doesn't work as the user would be left with specifying all the right headers. Do I assume some default?
+            response.setStatus(Ishiko::HTTPStatusCode::ok);
             response.body() = "Hello World!";
         }
     ));
