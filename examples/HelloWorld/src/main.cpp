6356ad116013998b3c9de110b380b5936238065b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-26 13:50:13 +0000

    Update HelloWorld example (#38)

--- examples/HelloWorld/src/main.cpp
+++ examples/HelloWorld/src/main.cpp
@@ -4,21 +4,21 @@
     See https://github.com/nemu-cpp/nemu/blob/main/LICENSE.txt
 */
 
-#include <Ishiko/HTTP.hpp>
 #include <Nemu.hpp>
 #include <iostream>
-#include <memory>
 
 int main(int argc, char* argv[])
 {
     Ishiko::Error error;
 
-    std::shared_ptr<Nemu::IshikoServer> server =
-        std::make_shared<Nemu::IshikoServer>(Ishiko::TCPServerSocket::AllInterfaces, Ishiko::Port::http, error);
+    // TODO: use the async server
+    std::shared_ptr<Nemu::IshikoSingleConnectionServer> server =
+        std::make_shared<Nemu::IshikoSingleConnectionServer>(Ishiko::TCPServerSocket::AllInterfaces,
+            Ishiko::Port::http, error);
 
     // Create a log that sends its output to the console.
     Ishiko::StreamLoggingSink sink(std::cout);
-    Nemu::Logger log(sink);
+    Ishiko::Logger log(sink);
 
     Nemu::WebApplication app(server, log, error);
     if (error)
@@ -26,15 +26,9 @@ int main(int argc, char* argv[])
         std::cout << "Error: " << error << std::endl;
     }
     
-    app.routes().append(Nemu::Route("/",
-        [](const Nemu::WebRequest& request, Nemu::WebResponseBuilder& response, void* handlerData,
-            Nemu::Logger& logger)
-        {
-            // TODO: this doesn't work as the user would be left with specifying all the right headers. Do I assume some default?
-            response.setStatus(Ishiko::HTTPStatusCode::ok);
-            response.body() = "Hello World!";
-        }
-    ));
+    app.routes().append(
+        Nemu::Route("/", 
+            std::make_shared<Nemu::HardcodedWebRequestHandler>(Ishiko::HTTPStatusCode::ok, "Hello World!")));
 
     app.start();
 
