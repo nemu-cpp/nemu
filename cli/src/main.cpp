58e0ddd9246d3c94b177afdf026055b20457d651
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-09-25 16:17:47 +0100

    Update main.cpp

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -1,7 +1,27 @@
 // SPDX-FileCopyrightText: 2020-2024 Xavier Leclercq
 // SPDX-License-Identifier: MIT
 
+#include <Ishiko/HTTP.hpp>
+#include <Ishiko/Networking.hpp>
+
 int main(int argc, char* argv[])
 {
+    Ishiko::Error error;
+
+    Ishiko::NetworkConnectionsManager connection_manager;
+
+    Ishiko::HTTPClient http_client{connection_manager};
+
+    Ishiko::IPv4Address address{"51.77.104.151", error};
+    
+    if (!error)
+    {
+        Ishiko::HTTPResponse response{Ishiko::HTTPStatusCode::internalServerError};
+        http_client.get(address, 8003, "/", response, error);
+
+        std::cout << response.toString() << std::endl;
+    }
+
+    // TODO: use proper error codes class
     return 0;
 }
