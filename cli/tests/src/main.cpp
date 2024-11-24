98db806cf1334e0e04c0b9bfbe748a59a97bd7e3
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-24 13:50:10 +0000

    Create main.cpp

---
+++ cli/tests/src/main.cpp
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: 2020-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#include <Ishiko/BasePlatform.hpp>
+#include <Ishiko/TestFramework.hpp>
+#include <exception>
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        Ishiko::TestHarness::CommandLineSpecification command_line_spec;
+        Ishiko::Configuration configuration = command_line_spec.createDefaultConfiguration();
+        configuration.set("context.data", "../../data");
+        configuration.set("context.reference", "../../reference");
+        configuration.set("context.output", "../../output");
+#if ISHIKO_OS == ISHIKO_OS_LINUX
+        configuration.set("context.application-path", "../../../../bin/NemuCLI");
+#elif ISHIKO_OS == ISHIKO_OS_WINDOWS
+        configuration.set("context.application-path", "../../../../bin/x64/NemuCLI.exe");
+#else
+#error Unsupported or unrecognized OS
+#endif
+        Ishiko::CommandLineParser::parse(command_line_spec, argc, argv, configuration);
+
+        Ishiko::TestHarness the_test_harness("CodeSmithy CLI Tests", configuration);
+
+        Ishiko::TestSequence& the_tests = the_test_harness.tests();
+
+        return the_test_harness.run();
+    }
+    catch (const std::exception& e)
+    {
+        return Ishiko::TestApplicationReturnCode::exception;
+    }
+    catch (...)
+    {
+        return Ishiko::TestApplicationReturnCode::exception;
+    }
+}
